8e87e444106502527f0828747d17e95d8255a1bf
Cavalex <42481044+Cavalex@users.noreply.github.com>
2018-09-25 00:11:22 +0100

    Updated Some Lists Names

--- Gear.py
+++ Gear.py
@@ -1,313 +1,201 @@
+import random
 
-add_attack = "Attack:"
-add_defense = "Defense:"
-add_HP = "HP added"
-add_MP = "MP added"
-rarity = "Rarity:"
+class GenMob:
+    type = ""
+    MAXHEALTH = 0
+    ATTACK = 0
+    DEFENSE = 0
+    HP = MAXHEALTH
 
+    def status(self):
+        print("Monster: {}".format(self.type))
+        print("HP: {}/{}".format(self.HP, self.MAXHEALTH))
+        print("Attack: {}".format(self.ATTACK))
+        print("Defense: {}".format(self.DEFENSE))
 
-DEFAULT_WEAPON = {
-    "Name:": "Rusty Sword",
-    add_attack: 1,
-    add_defense: 0,
-    rarity: 1
-}
+### Event Mobs:
 
-
-############################################################## Weapons:
-weapon_sword_wooden = {
-    "Name:": "Wooden Sword",
-    add_attack: 1.5,
-    add_defense: 0,
-    rarity: 1
-}
-
-weapon_sword_copper = {
-    "Name:": "Copper Sword",
-    add_attack: 1.5,
-    add_defense: 0,
-    rarity: 1
-}
-
-weapon_sword_tin = {
-    "Name:": "Tin Sword",
-    add_attack: 1.5,
-    add_defense: 0,
-    rarity: 1
-}
-
-weapon_sword_lead = {
-    "Name:": "Lead Sword",
-    add_attack: 1.5,
-    add_defense: 0,
-    rarity: 1
-}
-
-weapon_axe_wooden = {
-    "Name:": "Wooden Axe",
-    add_attack: 1.5,
-    add_defense: 0,
-    rarity: 1
-}
-
-weapon_dagger_copper = {
-    "Name:": "Copper Dagger",
-    add_attack: 1.5,
-    add_defense: 0,
-    rarity: 1
-}
-
-weapon_dagger_iron = {
-    "Name:": "Iron Dagger",
-    add_attack: 1.5,
-    add_defense: 0,
-    rarity: 1
-}
-
-weapon_sword_elven = {
-    "Name:": "Lost Elven Sword",
-    add_attack: 1.75,
-    add_defense: 0,
-    rarity: 1
-}
-
-weapon_sword_rusty_iron = {
-    "Name:": "Rusty Iron Sword",
-    add_attack: 1.75,
-    add_defense: 0,
-    rarity: 1
-}
-
-weapon_greatsword_wooden = {
-    "Name:": "Wooden Great Sword",
-    add_attack: 1.25,
-    add_defense: 0.25,
-    rarity: 1
-}
-
-weapon_axe_rusty_iron = {
-    "Name:": "Iron Axe",
-    add_attack: 1.75,
-    add_defense: 0,
-    rarity: 1
-}
-
-weapon_sword_iron = {
-    "Name:": "Iron Sword",
-    add_attack: 2,
-    add_defense: 0,
-    rarity: 2
-}
-
-weapon_greatsword_dwarven = {
-    "Name:": "Giant Dwarven Great Sword",
-    add_attack: 2,
-    add_defense: 0.25,
-    rarity: 2
-}
-
-weapon_sword_enchanted_iron = {
-    "Name:": "Enchanted Iron Sword",
-    add_attack: 2.25,
-    add_defense: 0,
-    rarity: 3
-}
-
-weapon_axe_iron = {
-    "Name:": "Iron Axe",
-    add_attack: 2,
-    add_defense: 0,
-    rarity: 2
-}
-
-WEAPONS = [weapon_sword_wooden, weapon_sword_iron, weapon_greatsword_wooden, weapon_sword_elven, weapon_sword_rusty_iron,
-           weapon_axe_wooden, weapon_axe_rusty_iron, weapon_sword_copper, weapon_sword_tin, weapon_sword_lead,
-           weapon_greatsword_dwarven, weapon_sword_enchanted_iron, weapon_dagger_copper, weapon_dagger_iron
-           ]
-
-##### Shop Weapons:
-
-weapon_sword_bronze = {
-    "Name:": "Bronze Sword",
-    add_attack: 1.75,
-    add_defense: 0,
-    rarity: 1
-}
-
-weapon_axe_bronze = {
-    "Name:": "Bronze Axe",
-    add_attack: 1.75,
-    add_defense: 0,
-    rarity: 1
-}
-
-weapon_longsword_iron = {
-    "Name:": "Iron Longsword",
-    add_attack: 2.25,
-    add_defense: 0,
-    rarity: 2
-}
-
-##### Event Weapons:
-
-weapon_sword_enchanted_gold = {
-    "Name:": "Enchanted Gold Sword",
-    add_attack: 2.5,
-    add_defense: 0,
-    rarity: 3
-}
-
-weapon_sword_gold = {
-    "Name:": "Gold Sword",
-    add_attack: 2,
-    add_defense: 0,
-    rarity: 2
-}
-
-weapon_sword_strongly_enchanted_iron = {
-    "Name:": "Strongly Enchanted Iron Sword",
-    add_attack: 2.5,
-    add_defense: 0,
-    rarity: 3
-}
-
-############################################################## Armor:
-
-##### Head:
-armor_head_rusty_cap = {
-    "Name:": "Rusty Cap",
-    "Type:": "Head",
-    add_defense: 0.5,
-    rarity: 1
-}
-
-armor_head_adventurer_helmet = {
-    "Name:": "Lost Adventurer's Helmet",
-    "Type:": "Head",
-    add_defense: 0.75,
-    rarity: 1
-}
-
-armor_head_bronze_helmet = {
-    "Name:": "Bronze Helmet",
-    "Type:": "Head",
-    add_defense: 0.75,
-    rarity: 1
-}
-
-armor_head_iron_barbute = {
-    "Name:": "Iron Barbute",
-    "Type:": "Head",
-    add_defense: 0.75,
-    rarity: 1
-}
-
-armor_head_elven_helmet = {
-    "Name:": "Lost Elven Helmet",
-    "Type:": "Head",
-    add_defense: 0.75,
-    rarity: 2
-}
-
-##### Chest:
-armor_chest_rusty_mail = {
-    "Name:": "Rusty Mail",
-    "Type:": "Chest",
-    add_defense: 0.5,
-    rarity: 1
-}
-
-armor_chest_adventurer_mail = {
-    "Name:": "Lost Adventurer's Mail",
-    "Type:": "Chest",
-    add_defense: 0.75,
-    rarity: 1
-}
-
-armor_chest_bronze_cuirass = {
-    "Name:": "Bronze Cuirass",
-    "Type:": "Chest",
-    add_defense: 0.75,
-    rarity: 1
-}
-
-armor_chest_iron_cuirass = {
-    "Name:": "Iron Cuirass",
-    "Type:": "Chest",
-    add_defense: 0.75,
-    rarity: 1
-}
-
-armor_chest_elven_breastplate = {
-    "Name:": "Lost Elven Breastplate",
-    "Type:": "Chest",
-    add_defense: 1,
-    rarity: 2
-}
-
-##### Legs:
-armor_legs_adventurers = {
-    "Name:": "Lost Adventurer's Leggings",
-    "Type:": "Legs",
-    add_defense: 0.25,
-    rarity: 1
-}
-
-armor_legs_rash_leggings = {
-    "Name:": "Rash Leggings",
-    "Type:": "Legs",
-    add_defense: 0,
-    rarity: 1
-}
-
-armor_legs_iron_greaves = {
-    "Name:": "Iron Greaves",
-    "Type:": "Legs",
-    add_defense: 0.25,
-    rarity: 1
-}
-
-armor_legs_bronze_greaves = {
-    "Name:": "Bronze Greaves",
-    "Type:": "Legs",
-    add_defense: 0.25,
-    rarity: 1
-}
-
-armor_legs_elven = {
-    "Name:": "Lost Elven Greaves",
-    "Type:": "Legs",
-    add_defense: 0.5,
-    rarity: 2
-}
+class TrapChest(GenMob):
+    name = "Trap Chest"
+    type = "a Trap Chest"
+    MAXHEALTH = 8
+    ATTACK = 2.5
+    DEFENSE = 2
+    HP = MAXHEALTH
 
 
-### Event Armor:
+### Mobs:
 
-armor_chest_dead_man_breastplate = {
-    "Name:": "Dead Adventurer's Breastplate",
-    "Type:": "Chest",
-    add_defense: 1.5,
-    rarity: 3
-}
+class Skeleton(GenMob):
+    name = "Skeleton"
+    type = "a Skeleton"
+    MAXHEALTH = 4
+    ATTACK = 2
+    DEFENSE = 0.5
+    HP = MAXHEALTH
+
+class GiantSlime(GenMob):
+    name = "Giant Slime"
+    type = "a Giant Slime"
+    MAXHEALTH = 6
+    ATTACK = 2
+    DEFENSE = 0.5
+    HP = MAXHEALTH
+
+class Goblin(GenMob):
+    name = "Goblin"
+    type = "a Goblin"
+    MAXHEALTH = 4
+    ATTACK = 2
+    DEFENSE = 1
+    HP = MAXHEALTH
+
+class UndeadWolf(GenMob):
+    name = "Undead Wolf"
+    type = "an Undead Wolf"
+    MAXHEALTH = 4
+    ATTACK = 1.5
+    DEFENSE = 1
+    HP = MAXHEALTH
+
+class Zombie(GenMob):
+    name = "Zombie"
+    type = "a Zombie"
+    MAXHEALTH = 5
+    ATTACK = 2
+    DEFENSE = 1
+    HP = MAXHEALTH
+
+####
+class PoisonousSlime(GenMob):
+    name = "Poisonous Slime"
+    type = "a Poisonous Slime"
+    MAXHEALTH = 3
+    ATTACK = 2.5
+    DEFENSE = 0.5
+    HP = MAXHEALTH
+
+class Orc(GenMob):
+    name = "Orc"
+    type = "an Orc"
+    MAXHEALTH = 6
+    ATTACK = 3
+    DEFENSE = 1
+    HP = MAXHEALTH
+#
+
+class HobGoblin(GenMob):
+    name = "Hobgoblin"
+    type = "a Hobgoblin"
+    MAXHEALTH = 8
+    ATTACK = 3
+    DEFENSE = 1.5
+    HP = MAXHEALTH
+
+class EnchantedSkeleton(GenMob):
+    name = "Enchanted Skeleton"
+    type = "an Enchanted Skeleton"
+    MAXHEALTH = 6
+    ATTACK = 2.5
+    DEFENSE = 2
+    HP = MAXHEALTH
+
+class HighOrc(GenMob):
+    name = "High Orc"
+    type = "a High Orc"
+    MAXHEALTH = 8
+    ATTACK = 4
+    DEFENSE = 1.5
+    HP = MAXHEALTH
+
+###
+class RedcapGoblin(GenMob):
+    name = "Redcap Goblin"
+    type = "a Redcap Goblin"
+    MAXHEALTH = 9
+    ATTACK = 4
+    DEFENSE = 1.5
+    HP = MAXHEALTH
+
+class MagicSpirit(GenMob):
+    rr = random.randint(1, 4)
+    if rr == 1:
+        type = "a Water Spirit"
+        name = "Water Spirit"
+    if rr == 2:
+        type = "a Fire Spirit"
+        name = "Fire Spirit"
+    if rr == 3:
+        type = "a Wind Spirit"
+        name = "Wind Spirit"
+    if rr == 4:
+        type = "an Earth Spirit"
+        name = "Earth Spirit"
+    MAXHEALTH = 4
+    ATTACK = 6
+    DEFENSE = 1
+    HP = MAXHEALTH
+#
+class EvolvedHighOrc(GenMob):
+    name = "Evolved High Orc"
+    type = "a Evolved High Orc"
+    MAXHEALTH = 9
+    ATTACK = 4
+    DEFENSE = 2
+    HP = MAXHEALTH
+
+class Elemental(GenMob):
+    rr = random.randint(1, 4)
+    if rr == 1:
+        type = "a Water Elemental"
+        name = "Water Elemental"
+    if rr == 2:
+        type = "a Fire Elemental"
+        name = "Fire Elemental"
+    if rr == 3:
+        type = "a Wind Elemental"
+        name = "Wind Elemental"
+    if rr == 4:
+        type = "an Earth Elemental"
+        name = "Earth Elemental"
+    MAXHEALTH = 5
+    ATTACK = 6
+    DEFENSE = 1.5
+    HP = MAXHEALTH
+
+class HiglyEnchantedSkeleton(GenMob):
+    name = "Highly Enchanted Skeleton"
+    type = "an Highly Enchanted Skeleton"
+    MAXHEALTH = 7
+    ATTACK = 3
+    DEFENSE = 2.5
+    HP = MAXHEALTH
+
+class Golem(GenMob):
+    rr = random.randint(1, 2)
+    if rr == 1:
+        type = "a Earth Golem"
+        name = "Earth Golem"
+    if rr == 2:
+        type = "a Iron Golem"
+        name = "Iron Golem"
+    MAXHEALTH = 10
+    ATTACK = 3
+    DEFENSE = 2.5
+    HP = MAXHEALTH
+
+class GiantPoisonousSlime(GenMob):
+    name = "Giant Poisonous Slime"
+    type = "a Giant Poisonous Slime"
+    MAXHEALTH = 3.5
+    ATTACK = 6
+    DEFENSE = 1
+    HP = MAXHEALTH
 
 
-ARMOR = [armor_head_adventurer_helmet, armor_chest_adventurer_mail, armor_legs_adventurers, armor_head_iron_barbute, armor_chest_iron_cuirass,
-         armor_legs_iron_greaves, armor_head_elven_helmet, armor_chest_elven_breastplate, armor_legs_elven, armor_head_bronze_helmet,
-         armor_chest_bronze_cuirass, armor_legs_bronze_greaves
-         ]
+ENEMIES_0_15 = [Goblin, UndeadWolf, Zombie, Skeleton, GiantSlime, Orc, PoisonousSlime]
 
-############################################################## Potions:
+ENEMIES_15_30 = [PoisonousSlime, Orc, HobGoblin, HighOrc, EnchantedSkeleton, MagicSpirit, RedcapGoblin]
 
-potion_hp = {
-    "Name:": "HP Potion,",
-    add_HP: 4
-}
-
-potion_mp = {
-    "Name:": "MP Potion,",
-    add_MP: 3
-}
-
-
-POTIONS = [potion_hp]
+ENEMIES_30_45 = [RedcapGoblin, MagicSpirit, EvolvedHighOrc, Elemental, Elemental, HiglyEnchantedSkeleton, Golem, GiantPoisonousSlime]
+# 2x Elementals. I like them lol.
